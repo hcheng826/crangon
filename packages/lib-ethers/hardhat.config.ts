f7af7192b480a71ffc0848467bac928bdf35d96b
hcheng826 <lilioo826@gmail.com>
2022-05-10 13:04:26 +0800

    set chainlink address

--- packages/lib-ethers/hardhat.config.ts
+++ packages/lib-ethers/hardhat.config.ts
@@ -80,11 +80,9 @@ const oracleAddresses = {
   },
   bsc: {
     chainlink: "0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE",
-    tellor: ""
   },
   bsctestnet: {
-    chainlink: "",
-    tellor: ""
+    chainlink: "0x2514895c72f50D8bd4B4F9b1110F0D6bD2c97526",
   }
 };
 
